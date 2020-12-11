3969e916c7ce8922cddfa53bba3570cfe0a636da
Ruslan Sayfutdinov <sayfutdinov@fb.com>
2020-12-11 02:04:44 +0000

    Adjust file length for github workflow

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ SERDE_DERIVE=$(find target/debug/deps -name 'serde_derive*.dll')
 # SERDE_DERIVE="target/debug/deps/serde_derive-88c54e7687dc6252.dll"
 rustc --crate-name=a --crate-type=rlib --edition=2018 --out-dir rust-out/a --extern=serde=$LIBSERDE --extern=serde_derive=$SERDE_DERIVE -Ldependency=target/debug/deps a/lib.rs
 
-rustc --crate-type=staticlib --edition=2018 --out-dir rust-out/b @b/argsfile-minus-few-lines.txt b/lib.rs
+rustc --crate-type=staticlib --edition=2018 --out-dir rust-out/b @b/argsfile-short.txt b/lib.rs
 
 rustc --crate-type=staticlib --edition=2018 --out-dir rust-out/b @b/argsfile.txt b/lib.rs
 
